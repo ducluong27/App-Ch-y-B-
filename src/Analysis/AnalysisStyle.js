1f2fbf457143bc6dd1d4d31b56bc4e9fb25a1ddb
ducluong27 <luong27052001@gmail.com>
2022-03-03 11:35:39 +0700

    Update Lan1

---
+++ src/Analysis/AnalysisStyle.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components'
+export const BackgroundAnalysis = styled.div`
+background-image: linear-gradient(-90deg,#919191, #acf7f7, #f7f5aa,#8cf773,#f7a3af);
+`
--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
-
+import MainContentBG from './MainContent/MainContentBG';
+import Navbar from './NavbarApp/Navbar';
+import Analysis from './Analysis/Analysis';
+import Profile from './Profile/Profile';
+import Statistical from './Statistical/Statistical'
+import RecentArchievement from './Archivement/RecentArchievement';
+import AddArchievement from './Archivement/AddArchievement';
+import Footer from './Footer/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<MainContentBG/>}/>
+        <Route path='/phan-tich' element={<Analysis/>}/>
+        <Route path='/nguoi-dung' element={<Profile/>}/>
+        <Route path='/thong-ke' element={<Statistical/>}/>
+        <Route path='/thanh-tich' element={<RecentArchievement/>}/>
+        <Route path='/them-thanh-tich' element={<AddArchievement/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
